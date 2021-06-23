a5858b12a8f0e9ace60b3d3fba6852f9c34b9d55
sarabastian <seb118@georgetown.edu>
2021-06-23 16:05:56 -0400

    admin

--- backend/booktracker/admin.py
+++ backend/booktracker/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book, Author
 
-# Register your models here.
+
+# models_list = [Book, Author]
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'read', 'author')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(Author, AuthorAdmin)
