9a62e2fbf4a38b34fdb157d59a53ad45d89c3d2f
sarabastian <seb118@georgetown.edu>
2021-06-23 18:08:05 -0400

    nested bauthor in book list

--- backend/booktracker/models.py
+++ backend/booktracker/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+# class AuthorManager(models.Model):
+# def get_by_natural_key(self, first_name, last_name):
+#     return self.get(first_name=first_name, last_name=last_name)
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
     def _str_(self):
         return self.first_name
+
+    # objects = AuthorManager()
+
+    # class Meta:
+    #     unique_together = [['first_name', 'last_name']]
 
 
 class Book(models.Model):
---
+++ backend/booktracker/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    # books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ('first_name', 'last_name')
+
+
+class BookSerializer(serializers.ModelSerializer):
+    # author = serializers.ReadOnlyField(source='author.first_name')
+    author = AuthorSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ['title', 'description', 'read', 'author']
